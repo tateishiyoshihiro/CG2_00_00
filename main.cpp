00eb37b1a7d238290b183651872af2f2ad9ddbdc
k022g0065@g.neec.ac.jp <k022g0065@g.neec.ac.jp>
2023-04-18 12:35:50 +0900

    4/18 00_03 4ページ目まで

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <Windows.h>
-//Windows�A�v���ł̃G���g���[�|�C���g(main�֐�)
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+//Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	//�o�̓E�B���h�E�ւ̕����o��
+	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
 
 	return 0;
