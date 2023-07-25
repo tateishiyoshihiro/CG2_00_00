6dc1c4acb5d4e22c42959f3e8fd20bb230497f9e
k022g0065@g.neec.ac.jp <k022g0065@g.neec.ac.jp>
2023-07-25 12:43:58 +0900

    2_4

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,4 +1,4 @@
-#include "object3d.hlsli"
+
 struct Material {
 	float32_t4 color;
 };
@@ -6,11 +6,5 @@ ConstantBuffer<Material>gMaterial:register(b0);
 struct PixelShaderOutput {
 	float32_t4 color : SV_TARGET0;
 };
-PixelShaderOutput main(VertexShaderOutput input) {
-	PixelShaderOutput output;
-	output.color = gMaterial.color;
-	output.texcoord = input.texcoord;
-	return output;
-}
 Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler :register(s0);
--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,19 +1,13 @@
-#include "Object3d.hlsli"
+
 struct TransfomationMatrix{
     float32_t4x4 WVP;
 };
-
 ConstantBuffer<TransfomationMatrix>gTransformationMatrix : register(b0);
 struct VertexShaderOutput {
 	float32_t4 position : SV_POSITION;
 };
-struct VertexShaderInput {
-	float32_t4 position : POSITION0;
-	float32_t2 texcoord : TEXCOORD0;
-};
 VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
 	output.position = mul(input.position,gTransformationMatrix.WVP);
-	output.texcoord = input.texcoord;
 	return output;
 }
