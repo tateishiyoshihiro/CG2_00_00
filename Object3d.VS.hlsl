be1b06646ceaac302f8611815e22efda2a4e7e39
k022g0065@g.neec.ac.jp <k022g0065@g.neec.ac.jp>
2023-07-25 12:44:53 +0900

    2_4

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,13 +1,15 @@
-
 struct TransfomationMatrix{
     float32_t4x4 WVP;
 };
+
 ConstantBuffer<TransfomationMatrix>gTransformationMatrix : register(b0);
 struct VertexShaderOutput {
 	float32_t4 position : SV_POSITION;
 };
+
 VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
 	output.position = mul(input.position,gTransformationMatrix.WVP);
+	
 	return output;
 }
