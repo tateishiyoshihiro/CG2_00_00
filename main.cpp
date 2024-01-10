38d520fe0a81d71afd63fbfc9f14b38df4622f12
立石義弘 <115062998+tateishiyoshihiro@users.noreply.github.com>
2024-01-10 10:51:10 +0900

    変更(異質な物)

--- main.cpp
+++ main.cpp
@@ -688,13 +688,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;//CBVを使う
 	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;//VertexShaderで使う
 	rootParameters[1].Descriptor.ShaderRegister = 0;//レジスタ番号0を使う
-	rootParameters[1].DescriptorTable.pDescriptorRanges = descriptorRangeForlnstancing;//Tableの中身の配列を指定
-	rootParameters[1].DescriptorTable.NumDescriptorRanges = _countof(descriptorRangeForlnstancing);//Tableで利用する数
 
 	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;//DescriptorTableを使う
 	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;//PixelShaderで使う
 	rootParameters[2].DescriptorTable.pDescriptorRanges = descriptorRange;//Tableの中身の配列を指定
 	rootParameters[2].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange);//Tableで利用する数
+	 
+	
+	//rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;    // CBVを使う
+	//rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;   // PixelShaderで使う
+	//rootParameters[0].Descriptor.ShaderRegister = 0;    // レジスタ番号0とバインド
+	// 
+	//rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; //CBVを使う
+	//rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX; //VertexShaderで使う
+	//rootParameters[1].Descriptor.ShaderRegister = 0; //レジスタ番号0を使う
+	//rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; // DescriptorTableを使う
+	//rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+	//rootParameters[2].DescriptorTable.pDescriptorRanges = descriptorRange;  // Tableの中身の配列を指定
+	//rootParameters[2].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange); // Tableで利用する数
+
 	descriptionRootSignature.pParameters = rootParameters;//ルートパラメーター配列へのポインタ
 	descriptionRootSignature.NumParameters = _countof(rootParameters);//配列の長さ
 	//シリアライズしてバイナリにする
@@ -1037,7 +1049,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			//
 			//commandList->SetGraphicsRootDescriptorTable(1, instancingSrvHandleGPU);
 			//
-			commandList->DrawInstanced(kVertexCount, kNumInstance, 0, 0);
+			commandList->DrawInstanced(kVertexCount, 1, 0, 0);
 			//
 			//commandList->DrawInstanced(UINT(),instanceCount);
 			//開発用のUIの処理、実際に開発用のUIを出す場合はここをゲーム固有の処理に置き換える
